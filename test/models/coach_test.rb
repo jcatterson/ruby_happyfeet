2d2cfebb229d08f9597af541dcfbe7c7fae560e3
Justin Catterson <justin.catterson@gmail.com>
2014-04-19 19:50:15 -0400

    Test for the full name

--- test/models/coach_test.rb
+++ test/models/coach_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class CoachTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "first name and last name" do
+    coach = Coach.new :first_name=>'test', :last_name=>'test'
+    assert_equal 'test test', coach.full_name, 'We expect the first and last name to be concatenated with a space in between'
+  end
 end
