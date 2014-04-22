3bc8aa8291258937e6c01f23815c108d59edfed7
Justin Catterson <justin.catterson@gmail.com>
2014-04-22 09:07:43 -0400

    Moving the find method into application as it eventually should be used by all my models for lookups

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,28 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def find
+    @found = []
+    if params[:school_name]
+      cls = constantize_with_care([School], "School")
+      @found = cls.by_like_name params[:school_name]
+    end
+    
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+  
+  def constantize_with_care(list_of_klasses = [], cls = "" )
+    list_of_klasses.each do |klass|
+      if cls == klass.to_s
+        return cls.constantize
+      end
+    end
+    
+    raise "Not allowed to constantize #{cls}"
+
+  end
 end
--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -12,32 +12,6 @@ class SchoolsController < ApplicationController
   def show
   end
 
-  # GET /schools/find
-  # POST /schools/find  
-  def find
-    @found = []
-    if params[:school_name]
-      cls = constantize_with_care([School], "School")
-      @found = cls.by_like_name params[:school_name]
-    end
-    
-    respond_to do |format|
-      format.html
-      format.js
-    end
-  end
-  
-  def constantize_with_care(list_of_klasses = [], cls = "" )
-    list_of_klasses.each do |klass|
-      if cls == klass.to_s
-        return cls.constantize
-      end
-    end
-    
-    raise "Not allowed to constantize #{cls}"
-
-  end
-
   # GET /schools/new
   def new
     @school = School.new
