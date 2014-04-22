0ad1e30c3ad83dc6c1d59024dfa23cc9ff986ef6
Justin Catterson <justin.catterson@gmail.com>
2014-04-21 23:30:47 -0400

    Only allowing one 'finder' on the page

--- app/assets/javascripts/students.js.coffee
+++ app/assets/javascripts/students.js.coffee
@@ -1,3 +1,10 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+$ ->
+    $(".lookup_icon").click ->
+        $(".finder").dialog "open"
+
+    $(".finder").not(":eq(0)").remove()
+    $(".finder").dialog({autoOpen: false})
+
--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,12 +1,12 @@
 module StudentsHelper
+
   def lookup( student )
     html = ""
     if student != nil && student.school != nil
       html << ( link_to student.school.name, url_for( student.school ) ).to_s
     end
-    
-    html << ( image_tag '/lookup.png', {class: 'lookup_icon'} ).to_s
-    html << "<div>" << (render partial: "schools/find").to_s << "</div>"
+    html << ( image_tag '/lookup.png', {class: 'lookup_icon' } ).to_s
+    html << "<div class='finder'>" << (render partial: "schools/find").to_s << "</div>"
     html.html_safe
   end
 end
