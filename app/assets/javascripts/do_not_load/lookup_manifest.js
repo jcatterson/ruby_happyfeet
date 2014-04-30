c4b5d797cccaecf0ee484071d733274f885c4c5c
Justin Catterson <justin.catterson@gmail.com>
2014-04-30 00:53:58 -0400

    I may have finally figured out adding more manifests

---
+++ app/assets/javascripts/do_not_load/lookup_manifest.js
@@ -0,0 +1,1 @@
+//= require do_not_load/lookup
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Happyfeet::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w(lookup.js)
+  config.assets.precompile += ['do_not_load/lookup_manifest.js']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
