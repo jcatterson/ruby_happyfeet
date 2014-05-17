d59394cc14da523e55249a8f12ded5b1e556c4b8
Justin Catterson <justin.catterson@gmail.com>
2014-05-17 17:42:52 -0400

    Lets null out the session user id if you log in, the attempt to log in again and mess up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to coaches_url
     else
+      session[:user_id] = nil
       redirect_to login_url, :alert =>'Invalid user/password'
     end
   end
